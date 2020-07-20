908b9039dcaf31656e7c186cf3b604eb7c049531
onilinkman <marbanchristian@gmail.com>
2020-07-20 19:40:59 -0400

    Se creo la carpeta donde se almacenaran las funciones para los endpoints y contiene a handlersProfesor que por ahora solo obtiene los profesores

---
+++ handlers/api/v1/handlersProfesor.go
@@ -0,0 +1,37 @@
+package v1
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"../../../models"
+	"github.com/gorilla/mux"
+)
+
+//GetProfesor obtiene un profesor
+func GetProfesor(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("holassss")
+	if profesor, err := getProfesorByCi(r); err != nil {
+		models.SendNotFound(w)
+	} else {
+		models.SendData(w, profesor)
+	}
+}
+
+//GetProfesores obtiene todos los profesores
+func GetProfesores(w http.ResponseWriter, r *http.Request) {
+	models.SendData(w, models.GetProfesores())
+}
+
+func getProfesorByCi(r *http.Request) (*models.Profesor, error) {
+	vars := mux.Vars(r)
+	num, _ := strconv.Atoi(vars["ci"])
+	profesor := models.GetProfesor(num)
+	if profesor.Ci == 0 {
+		return nil, errors.New("la ci de profesor no se encuentra en la base de datos")
+	}
+
+	return profesor, nil
+}
