661e36e89225f962b0d5da7e8cf8a440fdd9b908
onilinkman <marbanchristian@gmail.com>
2020-07-21 03:03:08 -0400

    se crea el archivo profesor.go en handlers para hacer acciones como crear eliminar o modificar al profesor en la base de datos cuando el cliente solicite dicha accion

---
+++ handlers/profesor.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"../models"
+	"../utils"
+)
+
+//NuevoProfesor se obtiene los datos para ingresar un profesor a la base de datos
+func NuevoProfesor(w http.ResponseWriter, r *http.Request) {
+	contexto := make(map[string]interface{})
+	//fmt.Println(r)
+	num, err := strconv.Atoi(r.FormValue("ci"))
+	if r.Method == "POST" && err == nil {
+		profesor := models.Profesor{
+			Ci:          num,
+			Nombres:     r.FormValue("nombres"),
+			Paterno:     r.FormValue("paterno"),
+			Materno:     r.FormValue("materno"),
+			Correo:      r.FormValue("correo"),
+			Contrasenia: r.FormValue("contrasenia"),
+			Idue:        1,
+		}
+		profesor.IngresarProfesor()
+
+	}
+	utils.RenderTemplate(w, "profesor/new", contexto)
+}
