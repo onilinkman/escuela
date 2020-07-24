21049290f6e25260be722e146582dfb10bfb3cc7
onilinkman <marbanchristian@gmail.com>
2020-07-24 19:27:57 -0400

    se agrego un login y se agrego un metodo para crear cookies

--- handlers/profesor.go
+++ handlers/profesor.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -29,8 +30,41 @@ func NuevoProfesor(w http.ResponseWriter, r *http.Request) {
 	utils.RenderTemplate(w, "profesor/new", contexto)
 }
 
+//LoginProfesor logea al Profesor
+func LoginProfesor(w http.ResponseWriter, r *http.Request) {
+	contexto := make(map[string]interface{})
+	num, err := strconv.Atoi(r.FormValue("ci"))
+	if r.Method == "POST" && err == nil {
+		contrasenia := r.FormValue("contrasenia")
+		profesor := models.GetProfesor(num)
+
+		if profesor.Ci == 0 {
+			fmt.Println("No existe profesor")
+		} else {
+			fmt.Println(profesor.Ci)
+			if models.Comparar(profesor.Contrasenia, contrasenia) {
+				fmt.Println("Acceso consedido")
+				createCookie(w)
+			}
+		}
+
+	} else {
+		fmt.Println(r.Method)
+	}
+	utils.RenderTemplate(w, "profesor", contexto)
+}
+
 //MenuProfesor enlaza la direccion hacia la pagina profesor
 func MenuProfesor(w http.ResponseWriter, r *http.Request) {
 	contexto := make(map[string]interface{})
 	utils.RenderTemplate(w, "profesor", contexto)
+}
+
+func createCookie(w http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:  "cookie_profesor",
+		Value: "cookie_value",
+		Path:  "/",
+	}
+	http.SetCookie(w, cookie)
 }
